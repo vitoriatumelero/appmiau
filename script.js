e5b522185415d8251421d990749d940fada1defd
vitoriatumelero <vitoriatumelero1807@gmail.com>
2024-06-08 21:28:36 -0300

    atualização login

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ function logar(){
     var login= document.getElementById('email_login').value;
     var senha= document.getElementById('senha_login').value;
 
-    if (login === "vitoria" && senha === "vitoria") {
-        alert('Sucesso!');
-        window.location.href = "home.html";
-        return false; // Evita o envio padrão do formulário
-      } else {
-        alert('Credenciais inválidas. Por favor, tente novamente.');
-        return false; // Evita o envio padrão do formulário
-      }
+    //if (login === "vitoria" && senha === "vitoria") {
+        //alert('Sucesso!');//
+       // window.location.href = "home.html";//
+        //return false; // Evita o envio padrão do formulário
+     // } //else {
+        //alert('Credenciais inválidas. Por favor, tente novamente.');
+        //return false; // Evita o envio padrão do formulário//
+    //  }
     }
