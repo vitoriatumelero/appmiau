9c48c688f991bc1c002c8c249ae68af3d3555950
vitoriatumelero <vitoriatumelero1807@gmail.com>
2024-06-18 19:47:00 -0300

    att

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function logar(){
-    var login= document.getElementById('email_login').value;
-    var senha= document.getElementById('senha_login').value;
+    //var login= document.getElementById('email_login').value;
+    //var senha= document.getElementById('senha_login').value;
 
     //if (login === "vitoria" && senha === "vitoria") {
         //alert('Sucesso!');//
-       // window.location.href = "home.html";//
+    window.location.href = "home.html"
         //return false; // Evita o envio padrão do formulário
      // } //else {
         //alert('Credenciais inválidas. Por favor, tente novamente.');
