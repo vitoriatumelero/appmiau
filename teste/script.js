a455fdade90c262c23cb3e960d7c66012f453c11
vitoriatumelero <vitoriatumelero1807@gmail.com>
2024-08-15 13:18:47 -0300

    .

---
+++ teste/script.js
@@ -0,0 +1,13 @@
+function logar(){
+    //var login= document.getElementById('email_login').value;
+    //var senha= document.getElementById('senha_login').value;
+
+    //if (login === "vitoria" && senha === "vitoria") {
+        //alert('Sucesso!');//
+    window.location.href = "home.html"
+        //return false; // Evita o envio padrão do formulário
+     // } //else {
+        //alert('Credenciais inválidas. Por favor, tente novamente.');
+        //return false; // Evita o envio padrão do formulário//
+    //  }
+    }
