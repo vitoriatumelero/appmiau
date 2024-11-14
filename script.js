dad61e3ff60bd001df10243e7944646c980e39af
Vitória Tumelero <162608569+vitoriatumelero@users.noreply.github.com>
2024-11-14 13:50:03 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 function logar(){
-    //var login= document.getElementById('email_login').value;
-    //var senha= document.getElementById('senha_login').value;
+    var login= document.getElementById('email_login').value;
+    var senha= document.getElementById('senha_login').value;
 
-    //if (login === "vitoria" && senha === "vitoria") {
-        //alert('Sucesso!');//
+    if (login === "vitoria" && senha === "vitoria") {
+        alert('Sucesso!');//
     window.location.href = "home.html"
-        //return false; // Evita o envio padrão do formulário
-     // } //else {
-        //alert('Credenciais inválidas. Por favor, tente novamente.');
-        //return false; // Evita o envio padrão do formulário//
-    //  }
+        return false; // Evita o envio padrão do formulário
+    }
+    else {
+        alert('Credenciais inválidas. Por favor, tente novamente.');
+        return false; // Evita o envio padrão do formulário//
+      }
     }
